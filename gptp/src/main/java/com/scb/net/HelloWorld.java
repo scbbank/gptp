6f71938cf8c713c465c1b1f8c4352be32bf49213
Tharun Reddy <stharunkumar709@gmail.com>
2023-05-19 09:40:41 +0530

    Added source files and pom

---
+++ gptp/src/main/java/com/scb/net/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+		System.out.println("DevOps Project for AchiStar Technologies";);
+    }
+}
