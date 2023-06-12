7a096d2ff5b58e523fb24760937a1c52c6aba11e
stharunkumar709 <41684674+stharunkumar709@users.noreply.github.com>
2023-06-12 17:06:44 +0530

    Update JUnitHelloWorldTest.java
    setup

--- gptp/src/test/java/com/scb/net/JUnitHelloWorldTest.java
+++ gptp/src/test/java/com/scb/net/JUnitHelloWorldTest.java
@@ -10,30 +10,8 @@ import org.junit.Test;
  
 public class JUnitHelloWorldTest {
  
-    @BeforeClass
-    public static void beforeClass() {
-        System.out.println("Before Class");
-    }
- 
-    @Before
-    public void before() {
-        System.out.println("Before Test Case");
-    }
- 
-    @Test
-    public void isGreaterTest() {
-        System.out.println("Test");
-        JUnitHelloWorld helloWorld = new JUnitHelloWorld();
-        assertTrue("Num 1 is greater than Num 2", helloWorld.isGreater(4, 3));
-    }
- 
-    @After
-    public void after() {
-        System.out.println("After Test Case");
-    }
- 
-    @AfterClass
-    public static void afterClass() {
-        System.out.println("After Class");
-    }
+   public static void main(String[] args)
+   {
+       System.out.println("this is Test Class");
+   }
 }
