fd154e252f4491a3b58c56407f0111a6429935e9
stharunkumar709 <41684674+stharunkumar709@users.noreply.github.com>
2023-06-12 16:50:40 +0530

    Update HelloWorld.java
    removed unwanted ";" in the code

--- gptp/src/main/java/com/scb/net/HelloWorld.java
+++ gptp/src/main/java/com/scb/net/HelloWorld.java
@@ -4,6 +4,6 @@ import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-		System.out.println("DevOps Project for AchiStar Technologies";);
+		System.out.println("DevOps Project for AchiStar Technologies");
     }
 }
