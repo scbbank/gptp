cfc5a3c2f2ef8150de8aa59d52fa99e5b521bab7
stharunkumar709 <41684674+stharunkumar709@users.noreply.github.com>
2023-11-01 03:49:01 +0530

    Delete unwanted gptp directory

--- gptp/src/main/java/com/scb/net/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.scb.net;
-
-import org.joda.time.LocalTime;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-		System.out.println("DevOps Project for AchiStar Technologies");
-    }
-}
